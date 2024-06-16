ff5a64814341c99c4bd0824b62843cf4dae97a22
Ramiro García Cárdenas <u202120230@upc.edu.pe>
2024-06-15 20:34:41 -0500

    add moduloubicacion list

--- Giftservice/src/app/app.routes.ts
+++ Giftservice/src/app/app.routes.ts
@@ -1,7 +1,22 @@
 import { Routes } from '@angular/router';
+import { ModuloUbicacionComponent } from './components/country/modulo-ubicacion/modulo-ubicacion.component';
+import { CountryComponent } from './components/country/country.component';
+import { CreateCountryComponent } from './components/country/create-country/create-country.component';
+import { CityComponent } from './components/country/city/city.component';
+import { InsertCityComponent } from './components/country/city/insert-city/insert-city.component';
 
 export const routes: Routes = [
     {
-        
+        path: 'moduloUbicacion', component: ModuloUbicacionComponent,
+    children: [
+      {
+        path: 'paises', component: CountryComponent,
+        children: [{ path: 'registro', component: CreateCountryComponent }]
+      },
+      {
+        path: 'ciudades', component: CityComponent,
+        children: [{ path: 'registro', component: InsertCityComponent }]
+      },
+    ]
     }
 ];
